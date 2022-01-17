bb6c9ec08497dad8bb93b59246ed66872166bc8b
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2022-01-17 11:35:53 -0500

    complete refactor (#1)
    * clean files
    
    * add luacheck and github action
    
    * clean up
    
    * finishing touches
    
    * consistent indentation using tabs
    
    Co-authored-by: OgelGames <olliverdc28@gmail.com>

---
+++ crafts.lua
@@ -0,0 +1,90 @@
+minetest.register_craft({
+	type = "cooking",
+	output = "steel:plate_hard",
+	recipe = "steel:plate_soft",
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "steel:grate_hard",
+	recipe = "steel:grate_soft",
+})
+
+minetest.register_craft({
+	output = "steel:scrap 2",
+	recipe = {{"steel:strut"}}
+})
+
+minetest.register_craft({
+	output = "steel:scrap 2",
+	recipe = {{"steel:grate_soft"}}
+})
+
+minetest.register_craft({
+	output = "steel:scrap 2",
+	recipe = {{"steel:grate_hard"}}
+})
+
+minetest.register_craft({
+	output = "steel:scrap",
+	recipe = {{"steel:roofing"}}
+})
+
+minetest.register_craft({
+	output = "steel:scrap 4",
+	recipe = {{"steel:plate_soft"}}
+})
+
+minetest.register_craft({
+	output = "steel:scrap 4",
+	recipe = {{"steel:plate_hard"}}
+})
+
+if minetest.get_modpath("default") then
+	minetest.register_craft({
+		output = "steel:plate_soft 2",
+		recipe = {
+			{"default:steel_ingot", "default:steel_ingot"},
+			{"default:steel_ingot", "default:steel_ingot"},
+		}
+	})
+
+	minetest.register_craft({
+		output = "steel:grate_soft 3",
+		recipe = {
+			{"default:steel_ingot", "", "default:steel_ingot"},
+			{"default:steel_ingot", "", "default:steel_ingot"},
+		}
+	})
+
+	minetest.register_craft({
+		output = "steel:roofing 6",
+		recipe = {{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"}}
+	})
+
+	minetest.register_craft({
+		output = "steel:strut_mount",
+		recipe = {{"steel:strut", "default:steel_ingot"}}
+	})
+
+	minetest.register_craft({
+		output = "steel:strut_mount",
+		recipe = {{"streets:steel_support", "default:steel_ingot"}}
+	})
+
+	minetest.register_craft({
+		output = "default:iron_lump",
+		recipe = {{"steel:scrap", "steel:scrap"}}
+	})
+end
+
+if not minetest.get_modpath("streets") or not minetest.get_modpath("steelsupport") then
+	minetest.register_craft({
+		output = "steel:strut 5",
+		recipe = {
+			{"", "default:steel_ingot", ""},
+			{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"},
+			{"", "default:steel_ingot", ""},
+		}
+	})
+end
